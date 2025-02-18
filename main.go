29b830224faadfd9ee7d3180b487436afc1099d6
Eduardo Poleo <eduardopoleo@gmail.com>
2025-02-18 10:49:45 -0500

    Add simple wiring

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"engine/vec2"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	v := vec2.Vec2{X: 5, Y: 4}
+
+	fmt.Printf("this is the x: %d and y: %d", v.X, v.Y)
 }
---
+++ vec2/vec2.go
@@ -0,0 +1,6 @@
+package vec2
+
+type Vec2 struct {
+	X int
+	Y int
+}
